26895f9259c4d8b127947b45cd3ca4fcf930453c
Edwin van Driel <edwin@westside.nl>
2017-01-30 22:02:05 +0100

    First extension

---
+++ ListViewExtensions/EvD.ListViewExtensions.SampleApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EvD.ListViewExtensions.SampleApp
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+    {
+		public ObservableCollection<string> SampleCollection{ get; set; }
+
+		public MainPage()
+        {
+            this.InitializeComponent();
+
+			SampleCollection = new ObservableCollection<string>();
+        }
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			for (var idx = 0; idx < 1000; idx++)
+			{
+				SampleCollection.Add($"This is row number {idx}");
+			}
+
+			SampleListView.ItemsSource = SampleCollection;
+		}
+
+	}
+}
---
+++ ListViewExtensions/EvD.ListViewExtensions/AlternateRowColor.cs
@@ -0,0 +1,48 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace EvD.ListViewExtensions
+{
+	public static class AlternateRowColor
+	{
+		public static readonly DependencyProperty ColorProperty = DependencyProperty.RegisterAttached(
+			"Color",
+			typeof(Brush),
+			typeof(AlternateRowColor),
+			new PropertyMetadata(null, OnColorPropertyChanged));
+
+		public static Brush GetColor(DependencyObject obj)
+		{
+			return (Brush)obj.GetValue(ColorProperty);
+		}
+
+		public static void SetColor(DependencyObject obj, Brush value)
+		{
+			obj.SetValue(ColorProperty, value);
+		}
+
+		private static void OnColorPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+		{
+			ListViewBase listViewBase = (ListViewBase)sender;
+
+			listViewBase.ContainerContentChanging -= ContainerContentChanging;
+
+			if (ColorProperty != null)
+			{
+				listViewBase.ContainerContentChanging += ContainerContentChanging;
+			}
+		}
+
+		private static void ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+		{
+			var itemContainer = args.ItemContainer as ListViewItem;
+			var index = sender.IndexFromContainer(itemContainer);
+
+			if (index % 2 == 0)
+				itemContainer.Background = GetColor(sender);
+			else
+				itemContainer.Background = null;
+		}
+	}
+}
